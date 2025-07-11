6568bed53fc5df7d6c76f887e022d963f3643f72
Eddie Santiago <eddiesantiago122007@gmail.com>
2025-07-10 21:29:40 -0500

    refactor: Optimize code with padStart

--- Kata Exercises/credit-card-mask.js
+++ Kata Exercises/credit-card-mask.js
@@ -1,10 +1,16 @@
 //TODO: https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
 
+//First Solution
 function maskify(cc) {
     const len = cc.length;
     return len > 4 ? "#".repeat(len - 4) + cc.slice(len - 4) : cc;
 }
 
-console.log(maskify("4556364607935616"))
-console.log(maskify("1"))
-console.log(maskify("11111"))
+//Optimized Solution
+function maskifyWithPadding(cc) {
+    return cc.slice(-4).padStart(cc.length, "#")
+}
+
+console.log(maskifyWithPadding("4556364607935616"))
+console.log(maskifyWithPadding("1"))
+console.log(maskifyWithPadding("11111"))
